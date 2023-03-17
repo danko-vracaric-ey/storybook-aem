91c681e4521428eb495d428f291dbce38813a978
Stefan Stojanovic <stefan.stojanovic2@rs.ey.com>
2023-03-17 12:43:21 +0100

    Added RadioInput story

---
+++ src/stories/components/FormRadioInput/FormRadioInput.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormRadioInput from "./FormRadioInput";
+
+export default {
+  title: "Base/Form/FormRadio",
+  component: FormRadioInput,
+};
+
+const Template = (args) => <FormRadioInput {...args} />;
+
+export const Radios = Template.bind({});
+Radios.args = {};
