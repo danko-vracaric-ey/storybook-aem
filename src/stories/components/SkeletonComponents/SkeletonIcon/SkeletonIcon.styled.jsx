740e0e9bb43acee881c3c17be4819799979de402
Stefan Stojanovic <stefan.stojanovic2@rs.ey.com>
2023-03-24 09:13:25 +0100

    Added SkeletonIcon story

---
+++ src/stories/components/SkeletonComponents/SkeletonIcon/SkeletonIcon.styled.jsx
@@ -0,0 +1,73 @@
+import styled, { css } from "styled-components";
+
+export const StyledSkeletonIcon = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: 25px;
+  width: 25px;
+  ${({ size }) =>
+    (size === "medium" &&
+      css`
+        height: 48px;
+        width: 48px;
+      `) ||
+    (size === "large" &&
+      css`
+        height: 60px;
+        width: 60px;
+      `)}
+  ${({ rounded }) =>
+    (rounded == "chamfer" &&
+      css`
+        border-radius: 5px;
+      `) ||
+    (rounded === "full" &&
+      css`
+        border-radius: 50%;
+      `)}
+  background-color: rgba(206, 206, 206, 0.75);
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    ${({ size }) =>
+      (size === "medium" &&
+        css`
+          height: 48px;
+          width: 48px;
+        `) ||
+      (size === "large" &&
+        css`
+          height: 60px;
+          width: 60px;
+        `)}
+    background-color: rgba(78, 78, 78, 0.95);
+    animation: 1.5s skeleton-loading linear infinite 1s;
+  }
+
+  @keyframes skeleton-loading {
+    0% {
+      opacity: 0;
+      width: 0;
+    }
+
+    100% {
+      background-color: rgba(206, 206, 206, 0.75);
+      opacity: 1;
+      width: 25px;
+      ${({ size }) =>
+        (size === "medium" &&
+          css`
+            width: 48px;
+          `) ||
+        (size === "large" &&
+          css`
+            width: 60px;
+          `)}
+    }
+  }
+`;
